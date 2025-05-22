f7172bc7cbd248a318750874de94aa39187fc76c
Gonhub9 <ho_gok@naver.com>
2025-05-22 12:59:46 +0900

    Level 0 개미 군단

--- src/Main.java
+++ src/Main.java
@@ -52,4 +52,21 @@ public class Main {
             }
         }
     }
+
+    class Solution {
+        public int solution(int hp) {
+            int count = 0;
+
+            count += hp / 5;
+            hp %= 5;
+
+            count += hp / 3;
+            hp %= 3;
+
+            count += hp / 1;
+            hp %= 1;
+
+            return count;
+        }
+    }
 }
